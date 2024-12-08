edaa2071533ab7447089af76dc0ba3ae2c03864d
pathfindermilan <pathfinder.milan@protonmail.com>
2024-12-08 23:48:46 +0100

    [d8.2023] Part 2

--- 2023/day8/main.py
+++ 2023/day8/main.py
@@ -1,4 +1,7 @@
 import re
+from math import gcd
+from functools import reduce
+from math import lcm
 
 data = open('input.txt').read().strip()
 pathway, routes = data.split("\n\n")
@@ -23,10 +26,37 @@ while helper != "ZZZ":
         pathway_iter = iter(pathway)
         ch = next(pathway_iter)
 
-    side = 0 if ch == "L" else 1
-    helper = dict_routes[helper][side]
+    helper = dict_routes[helper][0 if ch == "L" else 1]
     ans += 1
 print(ans)
 
 # part 2
+all_arrays_with_counts = []
+all_A = [k for k in dict_routes.keys() if k.endswith("A")]
 
+for one_A in all_A:
+    pathway_iter = iter(pathway)
+    one_array_with_counts = []
+    helper = one_A
+    count = 0
+    keep_all_z_positions = []
+    while True:
+        while count == 0 or not helper.endswith("Z"):
+            try:
+                ch = next(pathway_iter)
+            except StopIteration:
+                pathway_iter = iter(pathway)
+                ch = next(pathway_iter)
+
+            helper = dict_routes[helper][0 if ch == "L" else 1]
+            count += 1
+        one_array_with_counts.append(count)
+        if helper not in keep_all_z_positions:
+            keep_all_z_positions.append(helper)
+            count = 0
+        elif helper in keep_all_z_positions:
+            break
+    all_arrays_with_counts.append(one_array_with_counts)
+
+periods = [i[0] for i in all_arrays_with_counts]
+print(reduce(lcm, periods))
